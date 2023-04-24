0093962480e13a3ee24a5544c149790d71a920db
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-04-24 12:20:08 +0100

    show meal image and title in stack widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/meal.dart';
+import '../widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   final String title;
@@ -38,7 +39,9 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+        ),
       );
     }
 
