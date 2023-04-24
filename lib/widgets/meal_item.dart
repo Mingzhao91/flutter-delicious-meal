7558fb0985d1e9b0963dce8c974722804055cfaf
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-04-24 13:08:48 +0100

    show schedule complexity and affordability of a meal

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
+import './meal_item_trait.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
@@ -10,6 +11,16 @@ class MealItem extends StatelessWidget {
     super.key,
     required this.meal,
   });
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +69,29 @@ class MealItem extends StatelessWidget {
                     const SizedBox(
                       height: 12,
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: complexityText,
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordabilityText,
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
